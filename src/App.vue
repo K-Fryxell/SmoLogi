2d954c5f9d608123db2beb78613120ce61465d63
iwaya masataka <glosbe_kh@softbank.ne.jp>
2020-05-31 19:08:12 +0900

    sample_2

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
   <div id="app">
-    <router-link to="/">About</router-link>|
-    <router-link to="/1">Msg</router-link>|
-    <router-link to="/2">V-For</router-link>|
-    <router-link to="/3">V-On</router-link>|
-    <router-link to="/4">V-Model</router-link>|
-    <router-link to="/5">V-If</router-link>
+    <router-link to="/">About</router-link>
     <router-view/>
   </div>
 </template>
--- src/views/sample.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-      <h1>おはよう</h1>
-    </div>
-</template>
