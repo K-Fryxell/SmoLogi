665fd4653e8db47af846e0667673535f965f8ba0
iwaya masataka <glosbe_kh@softbank.ne.jp>
2020-05-31 20:08:03 +0900

    ADD_vuetify

--- src/router/index.js
+++ src/router/index.js
@@ -3,45 +3,10 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [{
-        path: '/Home',
+        path: '/',
         name: 'Home',
         component: () =>
-            import ('../views/Home.vue')
-    },
-    {
-        path: '/',
-        name: 'About',
-        component: () =>
-            import ('../views/About.vue')
-    }, {
-        path: '/1',
-        name: 'Msg',
-        component: () =>
-            import ('../views/Msg.vue')
-    },
-    {
-        path: '/2',
-        name: 'For',
-        component: () =>
-            import ('../views/For.vue')
-    },
-    {
-        path: '/3',
-        name: 'On',
-        component: () =>
-            import ('../views/On.vue')
-    },
-    {
-        path: '/4',
-        name: 'Model',
-        component: () =>
-            import ('../views/Model.vue')
-    },
-    {
-        path: '/5',
-        name: 'If',
-        component: () =>
-            import ('../views/If.vue')
+            import ('../views/index.vue')
     }
 ]
 const router = new VueRouter({
---
+++ src/views/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <HelloWorld/>
+  </div>
+</template>
+<script>
+import HelloWorld from '../components/HelloWorld'
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components:{
+    HelloWorld
+  }
+}
+</script>
