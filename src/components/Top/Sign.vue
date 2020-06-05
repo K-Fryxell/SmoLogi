0a74de15cbf3f9c890ef460270057c602f399909
片山翼 <denon@katayamatsubasanoMacBook-Air.local>
2020-06-05 19:02:05 +0900

    katayama_registration_vuetify

---
+++ src/components/Top/Sign.vue
@@ -0,0 +1,24 @@
+<template>
+    <content>
+        <!-- ログインボタン -->
+        <v-btn text color="blue lighten-2">
+            ログイン
+        </v-btn>
+        
+        <!-- 新規登録ページボタン -->
+        <v-btn color="blue lighten-2" dark to="regist">
+            新規登録
+        </v-btn>
+    </content>
+</template>
+
+
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
