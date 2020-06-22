5948d3625181a7d95162a7de67ad7b2267b6fdc6
片山翼 <denon38msn@gmail.com>
2020-06-22 14:52:27 +0900

    katayama_vuex_nickname

--- src/router/index.js
+++ src/router/index.js
@@ -3,16 +3,23 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [{
-    path: '/',
-    name: 'Top',
-    component: () =>
-        import ('../views/top.vue')
-}, {
-    path: '/regist',
-    name: 'Registration',
-    component: () =>
-        import ('@/views/registration')
-}]
+        path: '/',
+        name: 'Top',
+        component: () =>
+            import ('../views/top.vue')
+    },
+    {
+        path: '/regist',
+        name: 'Registration',
+        component: () =>
+            import ('@/views/registration')
+    }, {
+        path: '/user_mypage',
+        name: 'UserMypage',
+        component: () =>
+            import ('@/views/user/mypage')
+    }
+]
 const router = new VueRouter({
     mode: 'history',
     base: process.env.BASE_URL,
---
+++ src/store/part/index.js
@@ -0,0 +1,44 @@
+export default ({
+    state: {
+        // パートナーデータ
+
+        // メールアドレス・パスワード
+        part_email: "",
+        part_pass: "",
+        // 氏名・かな
+        part_fname: "",
+        part_fname_kana: "",
+        part_name: "",
+        part_name_kana: "",
+        // ニックネーム
+        nickname: "",
+        // 住所
+        part_address: "",
+        // 口座へのアクセスフラグ
+        part_account_flg: 0,
+        // 銀行口座
+        part_account_number: 0,
+        // 車種
+        part_cartype_number: 0,
+        // パートナー顔画像
+        part_image: "",
+        // 通知内容(フィールドにより対応必須(増える可能性大5.6個))
+        part_notification_content: "",
+        // 通知数
+        part_notification_number: 0,
+        // 利用回数履歴
+        part_usage_history: 0,
+        // 給項目は後回し
+
+        // レビュー機能星など1-5段階（仮）・余裕あればコメントも
+        part_review_evaluation: 0,
+
+    },
+    getters: {},
+    mutations: {
+
+    },
+    actions: {
+
+    }
+})
