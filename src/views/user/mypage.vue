a6923216343ce34110f5cbcc5fde185871c301d8
片山翼 <denon38msn@gmail.com>
2020-07-03 16:46:39 +0900

    katayama_mypage_selectdata

--- src/views/user/mypage.vue
+++ src/views/user/mypage.vue
@@ -2,30 +2,49 @@
     <div>
         <v-card>ユーザーログイン</v-card>
         <v-btn @click="logout">ログアウト</v-btn>
+        {{allData}}
+        {{user}}
     </div>
 </template>
 <script>
+import firebase from 'firebase'
 export default {
-    computed: {
-        user() {
-        return this.$store.getters.user;
-        },
-        userStatus() {
-        // ログインするとtrue
-        return this.$store.getters.isSignedIn;
+    data() {
+        return {
+            allData: "",
+            user: {
+                id: "",
+                email: "",
+                name: "",
+                sex: ""
+            }
         }
+    },
+    computed: {
     },
     methods:{
         logout:function(){
-            //#####現在エラー有り#####
-            this.$store.logout()
+            firebase.auth().signOut()
         }
     },
     created:function(){
         // this.$store.onAuth()
-    // if(!this.$store.state.status) {
-    //     this.$router.push('/regist')
-    // }
-    },
+        // firebase.firestore().collection('comments').get().then(snapshot => {
+        //   snapshot.forEach(doc => {
+        //     //contentは要素
+        //     //pushは配列データそのもの
+        //     // this.allData.push(doc.data().content)
+        //     this.allData = doc().data().content
+        //   })
+        // })
+        firebase.firestore().collection('users').doc('001').get().then( doc => {
+            console.log(doc.data())
+            this.user.email = doc.data().email
+            this.user.id = doc.data().id
+            this.user.name = doc.data().name
+            this.user.sex = doc.data().sex
+        })
+        
+    }
 }
 </script>
