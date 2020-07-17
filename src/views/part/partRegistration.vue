81e5a175e309cbdfa13e95a07cf6446e4d48f8b4
iwakurakaito <kangaruuuu7@docomo.ne.jp>
2020-07-17 11:37:15 +0900

    part_registuser_インデント汚いって言われたからきれいにした

--- src/views/part/partRegistration.vue
+++ src/views/part/partRegistration.vue
@@ -1,191 +1,188 @@
 <template>
-  <v-content>
-    <v-card>
-      <v-card-title class="text-center justify-center">パートナーユーザ登録</v-card-title>
-      <v-card-text>
-        <v-container class="ma-0 pa-0" fluid>
-            <v-layout column>
-                <v-form ref="form" v-model="regist">
-                    <v-row justify="center">
-                        <v-flex lg3 xs4>
-                            <!-- 姓textarea -->
-                            <v-col lg="12" cols="12">
-                                <v-text-field
-                                v-model="firstname"
-                                :rules="nameRules"
-                                required
-                                label="姓"
-                            ></v-text-field>
-                            </v-col>
-                        </v-flex>
-                        <v-flex lg3 xs4>
-                            <!-- 名textarea -->
-                            <v-col lg="12" cols="12">
-                                <v-text-field
-                                v-model="lastname"
-                                :rules="nameRules"
-                                required
-                                label="名"
-                            ></v-text-field>
-                            </v-col>
-                        </v-flex>
-                    </v-row>
-                    <v-row justify="center">
-                        <v-flex lg3 xs4>
-                            <!-- セイtextarea -->
-                            <v-col lg="12" cols="12">
-                                <v-text-field
-                                v-model="firstkana"
-                                :rules="kanaRules"
-                                required
-                                label="セイ"
-                            ></v-text-field>
-                            </v-col>
-                        </v-flex>
-                        <v-flex lg3 xs4>
-                            <!-- メイtextarea -->
-                            <v-col lg="12" cols="12">
-                                <v-text-field
-                                v-model="lastkana"
-                                :rules="kanaRules"
-                                required
-                                label="メイ"
-                            ></v-text-field>
-                            </v-col>
-                        </v-flex>
-                    </v-row>
-                    <!-- 郵便番号textarea -->
-                    <v-row justify="center">
-                        <v-col lg="6" cols="8">
-                            <v-text-field
-                            v-model="post"
-                            :rules="postRules"
-                            label="郵便番号(ハイフン(-)を含めて入力してください)"
-                            required
-                            ></v-text-field>
-                        </v-col>
-                    </v-row>
-                    <!-- 住所textarea -->
-                    <v-row justify="center">
-                        <v-col lg="6" cols="8">
-                            <v-text-field
-                            v-model="address"
-                            :rules="nameRules"
-                            label="住所"
-                            required
-                            ></v-text-field>
-                        </v-col>
-                    </v-row>
-                    <!-- 電話番号textarea -->
-                    <v-row justify="center">
-                        <v-col lg="6" cols="8">
-                            <v-text-field
-                            v-model="tel"
-                            :rules="telRules"
-                            label="電話番号(ハイフン(-)を含めて入力してください)"
-                            required
-                            ></v-text-field>
-                        </v-col>
-                    </v-row>
-
-                    <!-- メールtextarea -->
-                    <v-row justify="center">
-                        <v-col lg="6" cols="8">
-                            <v-text-field
-                            v-model="email"
-                            :rules="emailRules"
-                            :counter="50"
-                            label="メールアドレス"
-                            required
-                            ></v-text-field>
-                        </v-col>
-                    </v-row>
-                    <!-- ユーザ名textarea -->
-                    <v-row justify="center">
-                        <v-col lg="6" cols="8">
-                            <v-text-field
-                            v-model="username"
-                            :rules="usernameRules"
-                            :counter="8"
-                            required
-                            label="ユーザ名"
-                        ></v-text-field>
-                        </v-col>
-                    </v-row>
-                    <!-- ここから顔写真の登録   -->
-                    <v-row class="ma-0 pa-0 blue" justify="center">
-                        <v-col lg="2">
-                            <v-row class="ma-0 pa-0 red" justify="center">
-                                <v-card-text style="justify-center" class="justify-center">
-                                    顔写真の登録
-                                </v-card-text>
-                            </v-row>
-                        </v-col>
-                    </v-row>
-                    <v-row justify="center">
-                        <v-avatar size="200">
-                            <img v-if="this.$store.state.img!='no_image' && !uploadImageUrl" 
-                                :src="this.$store.state.img"
-                                alt="アイコンa"
-                                style="border-radius: 8em;
-                                width:200px;
-                                height:200px;" 
-                            > -->
-                            <img v-if="!uploadImageUrl && this.$store.state.img=='no_image'"
-                                src="#"
-                                alt="アイコンb"
-                                style="border-radius: 8em;
-                                width:200px;
-                                height:200px;"
-                            >
-                            <img v-if="uploadImageUrl" 
-                                :src="uploadImageUrl"
-                                alt="アイコンc"
-                                style="border-radius: 8em;
-                                width:200px;
-                                height:200px;" 
-                            >
-                        </v-avatar>
-                    </v-row>
-                    <!-- ここからテスト -->
-                    <input
-                        style="display: none"
-                        ref="input"
-                        type="file"
-                        accept="image/*, image/*, image/*"
-                        @change="selectedFile()"
-                    >
-                    <!-- ここまでテスト -->
-                    <v-row class="pa-0 ma-0 mt-4" justify="center">
-                        <v-col class="ma-0 pa-0" cols="auto">
-                            <v-flex xs12>
-                                <!-- ファイルの選択 -->
-                                <v-btn elevation="2" color="grey liten-5 white--text" @click="btnclick()">画像変更</v-btn>
+    <v-content>
+        <v-card>
+            <v-card-title class="text-center justify-center">パートナーユーザ登録</v-card-title>
+            <v-container class="ma-0 pa-0" fluid>
+                <v-layout column>
+                    <v-form ref="form" v-model="regist">
+                        <v-row justify="center">
+                            <v-flex lg3 xs4>
+                                <!-- 姓textarea -->
+                                <v-col lg="12" cols="12">
+                                    <v-text-field
+                                    v-model="firstname"
+                                    :rules="nameRules"
+                                    required
+                                    label="姓"
+                                ></v-text-field>
+                                </v-col>
                             </v-flex>
-                        </v-col>
-                    </v-row>
-                    <!-- ここまで顔写真の登録 -->
+                            <v-flex lg3 xs4>
+                                <!-- 名textarea -->
+                                <v-col lg="12" cols="12">
+                                    <v-text-field
+                                    v-model="lastname"
+                                    :rules="nameRules"
+                                    required
+                                    label="名"
+                                ></v-text-field>
+                                </v-col>
+                            </v-flex>
+                        </v-row>
+                        <v-row justify="center">
+                            <v-flex lg3 xs4>
+                                <!-- セイtextarea -->
+                                <v-col lg="12" cols="12">
+                                    <v-text-field
+                                    v-model="firstkana"
+                                    :rules="kanaRules"
+                                    required
+                                    label="セイ"
+                                ></v-text-field>
+                                </v-col>
+                            </v-flex>
+                            <v-flex lg3 xs4>
+                                <!-- メイtextarea -->
+                                <v-col lg="12" cols="12">
+                                    <v-text-field
+                                    v-model="lastkana"
+                                    :rules="kanaRules"
+                                    required
+                                    label="メイ"
+                                ></v-text-field>
+                                </v-col>
+                            </v-flex>
+                        </v-row>
+                        <!-- 郵便番号textarea -->
+                        <v-row justify="center">
+                            <v-col lg="6" cols="8">
+                                <v-text-field
+                                v-model="post"
+                                :rules="postRules"
+                                label="郵便番号(ハイフン(-)を含めて入力してください)"
+                                required
+                                ></v-text-field>
+                            </v-col>
+                        </v-row>
+                        <!-- 住所textarea -->
+                        <v-row justify="center">
+                            <v-col lg="6" cols="8">
+                                <v-text-field
+                                v-model="address"
+                                :rules="nameRules"
+                                label="住所"
+                                required
+                                ></v-text-field>
+                            </v-col>
+                        </v-row>
+                        <!-- 電話番号textarea -->
+                        <v-row justify="center">
+                            <v-col lg="6" cols="8">
+                                <v-text-field
+                                v-model="tel"
+                                :rules="telRules"
+                                label="電話番号(ハイフン(-)を含めて入力してください)"
+                                required
+                                ></v-text-field>
+                            </v-col>
+                        </v-row>
 
-                    <!-- 次へボタン -->
-                    <v-row class="mt-6" justify="center">
-                        <v-btn
-                            class="grey liten-5 white--text"
-                            to="/part_credit"
-                            :disabled="!regist"
-                            @click="register()"
-                            width="170"
-                            height="50"
+                        <!-- メールtextarea -->
+                        <v-row justify="center">
+                            <v-col lg="6" cols="8">
+                                <v-text-field
+                                v-model="email"
+                                :rules="emailRules"
+                                :counter="50"
+                                label="メールアドレス"
+                                required
+                                ></v-text-field>
+                            </v-col>
+                        </v-row>
+                        <!-- ユーザ名textarea -->
+                        <v-row justify="center">
+                            <v-col lg="6" cols="8">
+                                <v-text-field
+                                v-model="username"
+                                :rules="usernameRules"
+                                :counter="8"
+                                required
+                                label="ユーザ名"
+                            ></v-text-field>
+                            </v-col>
+                        </v-row>
+                        <!-- ここから顔写真の登録   -->
+                        <v-row class="ma-0 pa-0 blue" justify="center">
+                            <v-col lg="2">
+                                <v-row class="ma-0 pa-0 red" justify="center">
+                                    <v-card-text style="justify-center" class="justify-center">
+                                        顔写真の登録
+                                    </v-card-text>
+                                </v-row>
+                            </v-col>
+                        </v-row>
+                        <v-row justify="center">
+                            <v-avatar size="200">
+                                <img v-if="this.$store.state.img!='no_image' && !uploadImageUrl" 
+                                    :src="this.$store.state.img"
+                                    alt="アイコンa"
+                                    style="border-radius: 8em;
+                                    width:200px;
+                                    height:200px;" 
+                                > -->
+                                <img v-if="!uploadImageUrl && this.$store.state.img=='no_image'"
+                                    src="#"
+                                    alt="アイコンb"
+                                    style="border-radius: 8em;
+                                    width:200px;
+                                    height:200px;"
+                                >
+                                <img v-if="uploadImageUrl" 
+                                    :src="uploadImageUrl"
+                                    alt="アイコンc"
+                                    style="border-radius: 8em;
+                                    width:200px;
+                                    height:200px;" 
+                                >
+                            </v-avatar>
+                        </v-row>
+                        <!-- ここからテスト -->
+                        <input
+                            style="display: none"
+                            ref="input"
+                            type="file"
+                            accept="image/*, image/*, image/*"
+                            @change="selectedFile()"
                         >
-                        次へ
-                        </v-btn>
-                    </v-row>
-                </v-form>
-            </v-layout>
-        </v-container>
-      </v-card-text>
+                        <!-- ここまでテスト -->
+                        <v-row class="pa-0 ma-0 mt-4" justify="center">
+                            <v-col class="ma-0 pa-0" cols="auto">
+                                <v-flex xs12>
+                                    <!-- ファイルの選択 -->
+                                    <v-btn elevation="2" color="grey liten-5 white--text" @click="btnclick()">画像変更</v-btn>
+                                </v-flex>
+                            </v-col>
+                        </v-row>
+                        <!-- ここまで顔写真の登録 -->
 
-    </v-card>
-  </v-content>
+                        <!-- 次へボタン -->
+                        <v-row class="mt-6" justify="center">
+                            <v-btn
+                                class="grey liten-5 white--text"
+                                to="/part_credit"
+                                :disabled="!regist"
+                                @click="register()"
+                                width="170"
+                                height="50"
+                            >
+                            次へ
+                            </v-btn>
+                        </v-row>
+                    </v-form>
+                </v-layout>
+            </v-container>
+        </v-card>
+    </v-content>
 </template>
 
 
