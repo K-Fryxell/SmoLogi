e5723cc90bd43ccb32a3160ca034a6f66d2ada41
osaki <kookm1@mkmnomacbookpro.local>
2020-07-22 04:17:53 +0900

    fix

--- src/views/user/change_detail.vue
+++ src/views/user/change_detail.vue
@@ -1,137 +1,178 @@
 <template>
 <v-container class="ma-0 pa-0" fluid>
+  <Uheader/>
   <v-layout class="ma-0 pa-0" wrap>
     <v-flex xs12 lg12 >
       <v-card class="ma-0 pa-0" elevation="0">
-        <v-card-title class="justify-center font-weight-light">
-          詳細変更
-        </v-card-title>
-            <v-flex xs12 lg12>
-              <v-row justify="center">
-                <v-col cols="7" class="mx-12">
-                  <v-form ref="form">
-                    <v-row>
-                      <v-col cols="6" lg="4">
-                        <v-text-field
-                          v-model="sei"
-                          label="姓"
-                          :rules="seiRules"
-                        ></v-text-field>
-                      </v-col>
-
-                      <v-col cols="6" lg="4">
-                        <v-text-field
-                          v-model="mei"
-                          label="名"
-                          :rules="meiRules"
-                        ></v-text-field>
-                      </v-col>
-                    </v-row>
-                    <v-row>
-                        <v-col cols="6" lg="4">
-                          <v-text-field
-                            v-model="seiKana"
-                            label="セイ"
-                            :rules="seikanaRules"
-                          ></v-text-field>
-                        </v-col>
-
-                        <v-col cols="6" lg="4">
-                          <v-text-field
-                            v-model="meiKana"
-                            label="メイ"
-                            :rules="meikanaRules"
-                          ></v-text-field>
-                        </v-col>
-                    </v-row>
-
-                    <v-row>
-                        <v-col cols="12" lg="5">
-                          <v-text-field
-                            v-model="post"
-                            label="郵便番号"
-                            :rules="postRules"
-                          ></v-text-field>
-                        </v-col>
-                    </v-row>
-
-                    <v-row>
-                        <v-col>
-                          <v-text-field
-                            v-model="address"
-                            label="住所"
-                            :rules="addressRules"
-                          ></v-text-field>
-                        </v-col>
-                    </v-row>
-
-                    <v-row>
-                        <v-col>
-                          <v-text-field
-                            v-model="mailaddress"
-                            counter
-                            label="メールアドレス"
-                            :rules="mailRules"
-                            hint="メールアドレスは50字以下で記入してください。"
-                          ></v-text-field>
-                        </v-col>
-                    </v-row>
-
-                    <v-row>
-                        <v-col>
-                          <v-text-field
-                            v-model="tel"
-                            label="電話"
-                            :rules="telRules"
-                          ></v-text-field>
-                        </v-col>
-                    </v-row>
-                  </v-form>
-                </v-col>
-              </v-row>
-            </v-flex>
+        <v-card-title class="justify-center font-weight-thin">
+          詳細変更</v-card-title>
+          <v-flex xs12 lg12>
+            <v-row justify="center" class="ma-0 pa-0">
+              <v-col cols="auto" lg="9" class="mx-12">
+                <v-form ref="form" v-model="update">
+                  <v-row class="ma-0 pa-0">
+                    <v-col cols="6" lg="4">
+                      <v-text-field
+                        prepend-icon="mdi-account-circle"
+                        v-model="firstname"
+                        label="姓"
+                        :rules="firstnameRules"/>
+                    </v-col>
+                    <v-col cols="6" lg="4">
+                      <v-text-field
+                        v-model="lastname"
+                        label="名"
+                        :rules="lastnameRules"/>
+                    </v-col>
+                  </v-row>
+                  <v-row class="ma-0 pa-0">
+                    <v-col cols="6" lg="4">
+                      <v-text-field
+                        prepend-icon="mdi-account-circle-outline"
+                        v-model="firstkana"
+                        label="セイ"
+                        :rules="firstkanaRules"/>
+                    </v-col>
+                    <v-col cols="6" lg="4">
+                      <v-text-field
+                        v-model="lastkana"
+                        label="メイ"
+                        :rules="lastkanaRules"/>
+                    </v-col>
+                  </v-row>
+                  <v-row class="ma-0 pa-0">
+                    <v-col cols="12" lg="4">
+                      <v-radio-group
+                        prepend-icon="mdi-human-male-female"
+                        v-model="sex" :mandatory="false" row>
+                        <v-radio label="男性"/>
+                        <v-radio label="女性"/>
+                      </v-radio-group>
+                    </v-col>
+                  </v-row>
+                  <v-row class="ma-0 pa-0">
+                    <v-col>
+                      <v-text-field
+                        prepend-icon="mdi-email"
+                        v-model="email"
+                        counter
+                        label="メールアドレス"
+                        :rules="emailRules"
+                        hint="メールアドレスは50字以内で記入してください。"/>
+                    </v-col>
+                  </v-row>
+                  <v-row class="ma-0 pa-0">
+                    <v-col cols="12" lg="5">
+                      <v-text-field
+                        prepend-icon="mdi-currency-kzt"
+                        v-model="post"
+                        label="郵便番号"
+                        :rules="postRules"
+                        hint="郵便番号は(-)を含んで入力してください。"/>
+                    </v-col>
+                  </v-row>
+                  <v-row class="ma-0 pa-0">
+                    <v-col>
+                      <v-text-field
+                        prepend-icon="mdi-home"
+                        v-model="address"
+                        label="住所"
+                        :rules="addressRules"/>
+                    </v-col>
+                  </v-row>
+                  <v-row class="ma-0 pa-0">
+                    <v-col>
+                      <v-text-field
+                        prepend-icon="mdi-phone-in-talk"
+                        v-model="tel"
+                        label="電話"
+                        :rules="telRules"
+                        hint="電話番号は(-)を含んで入力してください。"/>
+                    </v-col>
+                  </v-row>
+                </v-form>
+                <v-row justify="center" class="ma-0 pa-0">
+                  <v-btn
+                    color="grey darken-2"
+                    outlined
+                    x-large
+                    :disabled="!update"
+                    @click="updat()"
+                    >
+                    更新
+                  </v-btn>
+                </v-row>
+                <v-row justify="center"
+                  class="ma-0 pa-0">
+                  <v-btn
+                    class="mt-12"
+                    color="grey darken-2"
+                    outlined
+                    x-large
+                    rounded
+                    >
+                    クレジットカード情報変更
+                  </v-btn>
+                </v-row>
+                <v-row justify="center" class="ma-0 pa-0">
+                  <v-btn
+                    class="mt-12"
+                    color="grey darken-2"
+                    outlined
+                    x-large
+                    rounded
+                    >
+                    パスワード変更
+                  </v-btn>
+                </v-row>
+              </v-col>
+            </v-row>
+          </v-flex>
       </v-card>
-      <v-row justify="center">
-          <v-btn class="mt-12" color="primary"  x-large>変更</v-btn>
-      </v-row>
-      <div class="my-2">
-        <v-btn rounded class="mt-12" color="primary"  x-large>クレジットカード情報変更</v-btn>
-      </div>
-
-      <div class="my-2 ">
-        <v-btn rounded class="mt-12" color="primary"  x-large>パスワード変更</v-btn>
-      </div>
-
     </v-flex>
   </v-layout>
+  <Ufooter/>
 </v-container>
 </template>
 <script>
+import Uheader  from '../../components/User/Uheader'
+import Ufooter from '../../components/User/Ufooter'
 export default {
     data(){
         return{
-          sei: '',
-          mei: '',
-          seiKana: '',
-          meiKana: '',
+          firstname: '',
+          lastname: '',
+          firstkana: '',
+          lastkana: '',
+          sex: '',
+          email: '',
           post: '',
           address: '',
-          mailaddress: '',
           tel: '',
-          seiRules:[
+          array:{},
+          update :true,
+          //姓名
+          firstnameRules:[
             v => !!v || '入力欄が空白です。',
           ],
-          meiRules:[
+          lastnameRules:[
             v => !!v || '入力欄が空白です。',
           ],
           //セイメイ
-          seikanaRules: [
+          firstkanaRules: [
             v => !!v || '入力欄が空白です。',
             v => /[ァ-ヴ]$/.test(v)  ||'カタカナで入力してください。'
           ],
-          meikanaRules: [
+          lastkanaRules: [
             v => !!v || '入力欄が空白です。',
-            v => /[ァ-ヴ]$/.test(v) || 'カタカナで入力してください。'
+            v => /[ァ-ヴ]$/.test(v)  ||'カタカナで入力してください。'
+          ],
+          //メールアドレス
+          emailRules: [
+            v => !!v || '入力欄が空白です。',
+            v => /.+@.+\..+/.test(v) || 'メールアドレスの入力形式が異なっています。',
+            v => (v && v.length <= 50) || '50字以内で入力してください。',
+            v => /[a-zA-Z\d]$/.test(v) ||'半角英数字のみで入力してください。',
           ],
           //郵便番号
           postRules: [
@@ -144,13 +185,6 @@ export default {
             v => !!v || '入力欄が空白です。',
             v => /^[^A-Za-z0-]+$/.test(v) || '全角で入力してください。',
           ],
-          //メールアドレス
-          mailRules: [
-            v => !!v || '入力欄が空白です。',
-            v => /.+@.+\..+/.test(v) || 'メールアドレスの入力形式が異なっています。',
-            v => (v && v.length <= 50) || '50字以内で入力してください。',
-            v => /[a-zA-Z\d]$/.test(v) ||'半角英数字のみで入力してください。',
-          ],
           //電話番号
           telRules: [
             v => !!v || '入力欄が空白です。',
@@ -158,9 +192,20 @@ export default {
             v => /^0\d{1,4}-\d{1,4}-\d{4}$/.test(v) || /^0[789]0-[0-9]{4}-[0-9]{4}$/.test(v) || '電話番号の形式が違います'
           ],
         }
+    },method:{
+        validate(){
+          this.$refs.form.validate()
+      },
+        updat(){
+          this.array['firstname'] = this.firstname
+          this.array['lastname'] = this.lastname
+          this.array['email'] = this.email
+          this.array['address'] = this.address
+          this.array['tel'] = this.tel
+        }
+    },components: {
+      Ufooter,
+      Uheader,
     },
-    method: {
-
-    }
 }
 </script>
--- src/views/user/transport_info.vue
+++ src/views/user/transport_info.vue
@@ -1,81 +1,149 @@
 <template>
 <v-container class="ma-0 pa-0" fluid>
+  <Uheader/>
   <v-layout class="ma-0 pa-0" wrap>
     <v-flex xs12 lg12>
-        <v-card class="ma-0 pa-0" elevation="0">
-                <h2 class="pa-4 font-weight-light">配達希望時刻</h2>
-                  <v-row class="ma-3">
-                    <v-col cols="4" lg="2">
+        <v-card class="mt-12 pa-0" height="100%" elevation="0" style="width:1440px;">
+          <v-card-title class="justify-center">運搬情報</v-card-title>
+              <v-form ref="form" v-model="decision">
+                <v-row justify="center" class="ma-0 pa-0">
+                  <v-col cols="2" lg="2">
+                    <v-row justify="end">
+                        <v-col cols="auto">
+                          <h2 class="pt-2 display-6 font-weight-light">配達希望時刻</h2>
+                        </v-col>
+                    </v-row>
+                  </v-col>
+                    <v-col cols="auto" style="width:110px;">
                       <v-select
                         v-model="Time"
-                        :items="time">
-                      </v-select>
+                        :items="time"
+                        hide-details
+                        single-line
+                        />
+                    </v-col>
+                    <v-col cols="auto" class="mt-5 ml-n4">
+                    時
+                    </v-col>
+                    <v-col cols="auto" style="width:110px;">
+                      <v-select
+                        v-model="Minute"
+                        :items="minute"
+                        hide-details
+                        single-line
+                        />
+                    </v-col>
+                    <v-col cols="auto" class="mt-5 ml-n4">
+                      分
+                    </v-col>
+                  </v-row>
+
+                  <v-row justify="center" class="ma-0 pa-0">
+                    <v-col cols="2" >
+                      <v-row justify="end">
+                          <v-col cols="auto">
+                          <h2 class=" pt-2 font-weight-light" display-6>荷物の重量</h2>
+                        </v-col>
+                      </v-row>
+                    </v-col>
+                    <v-col cols="auto" style="width:110px;">
+                      <v-text-field v-model="weight"/>
+                    </v-col>
+                    <v-col cols="auto" class="mt-5 ml-n4">
+                      kg以下
                     </v-col>
 
-                    <div class="mt-10">
-                      時
-                    </div>
-
-                <v-col cols="4" lg="2">
-                  <v-select
-                    v-model="Minute"
-                    :items="minute"
-                  ></v-select>
-                </v-col>
-
-                <div class="mt-10">
-                  分
-                </div>
-              </v-row>
-            <h2 class="pa-4 font-weight-light">荷物の重量</h2>
-              <v-row class="ma-3">
-                <v-col cols="4" lg="2">
-                  <v-text-field v-model="weight"></v-text-field>
-                </v-col>
-
-                <div class="mt-10">
-                  kg以下
-                </div>
-              </v-row>
-              <v-row class="mx-6">
-                <v-col cols="2" lg="2">
-                  <v-btn @click="select(1)" rounded x-large color="primary"
-                  >1キロ</v-btn>
-                </v-col>
-
-                <v-col cols="2" lg="2">
-                  <v-btn @click="select(3)" rounded x-large color="primary">3キロ</v-btn>
-                </v-col>
-
-                <v-col cols="2" lg="2">
-                  <v-btn @click="select(5)" rounded x-large color="primary">5キロ</v-btn>
-                </v-col>
-              </v-row>
-              <div class="ma-3">
-                <v-btn x-large color="primary">決定</v-btn>
-              </div>
+                    <v-col cols="1"></v-col>
+                  </v-row>
+                    <v-row class="ma-0 pa-6" justify="center">
+                      <v-col cols="auto">
+                        <v-btn
+                          @click="select(1)"
+                          width="100"
+                        height="50"
+                          large
+                          outlined
+                          color="grey darken-2"
+                          >
+                          1キロ
+                        </v-btn>
+                      </v-col>
+                      <v-col cols="auto">
+                        <v-btn
+                          @click="select(3)"
+                        width="100"
+                        height="50"
+                          large
+                          outlined
+                          color="grey darken-2"
+                          >
+                          3キロ
+                        </v-btn>
+                      </v-col>
+                      <v-col cols="auto">
+                        <v-btn
+                        width="100"
+                        height="50"
+                          @click="select(5)"
+                          large
+                          outlined
+                          color="grey darken-2"
+                          >
+                          5キロ
+                        </v-btn>
+                      </v-col>
+                    </v-row>
+                    <v-row justify="center" class="mb-12 mt-5">
+                      <v-btn
+                        width="100"
+                        height="60"
+                        :disabled="!decision"
+                        x-large
+                        @click="decide"
+                      >
+                        決定
+                      </v-btn>
+          </v-row>
+        </v-form>
         </v-card>
     </v-flex>
   </v-layout>
+  <Ufooter/>
 </v-container>
 </template>
 <script>
+import Uheader  from '../../components/User/Uheader'
+import Ufooter from '../../components/User/Ufooter'
 export default {
     data(){
         return{
           Time:'1',
           time:[5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
-          Minute:'1',
+          Minute:"1",
           minute:[10,20,30,40,50],
-          weight:''
-
-        }
-    },
-    methods: {
-      select(kg){
-        this.weight=kg
+          weight:'',
+          x:0,
+          y:0,
+          size:"display-1",
+          array:{},
+          decision:true,
+          }
+        },methods:{
+          select(kg){
+            this.weight=kg
+        },
+          validate(){
+            this.$refs.form.validate()
+          },
+          decide(){
+            this.array['timenum']=this.Time
+            this.array['Minutenum']=this.Minute
+            this.array['weight']=this.weight
+            return this.$store.commit('decide', this.array)
+          }
+        },components: {
+          Ufooter,
+          Uheader,
+        },
       }
-
-    },
-}
 </script>
