294307d8fa273e77ddfa5bdefd922d8bec13c843
片山翼 <denon38msn@gmail.com>
2020-07-26 01:45:51 +0900

    katayama_contact_overlay

--- src/views/contact/contact.vue
+++ src/views/contact/contact.vue
@@ -1,119 +1,108 @@
 <template>
     <v-layout>
         <v-flex xs=12 sm=6 md=6>
-            <v-row class="ma-0 pa-0 mx-12">
+            <v-row class="ma-0 pa-0">
                 <v-col class="ma-0 pa-0">
-                    <v-row class="ma-0 pa-0">
-                        <v-col cols="9" class="ma-0 pa-0">
-                            <v-card class="red pa-5" elevation="0" height="700" tile>
-                                <v-card class="green" elevation="0" height="650" tile >
-                                    <!-- ここにグーグルマップの表示 -->
-                                    <GmapMap
-                                        :center="{lat:user_latitude, lng:user_longitude}"
-                                        :zoom="zoom"
-                                        style="width: 100%; height: 100%;"
-                                        :options="mapStyle">
+                    <v-row class="ma-0 pa-0" justify="center">
+                        <v-col cols="12" class="ma-0 pa-0">
+                            <v-card class="green" elevation="0" height="650" tile >
+                                    <!-- ここにグーグルマップの表示  -->
+                                <GmapMap
+                                    :center="{lat:user_latitude, lng:user_longitude}"
+                                    :zoom="zoom"
+                                    style="width: 100%; height: 100%;"
+                                    :options="mapStyle">
 
-                                        <GmapInfoWindow
-                                            :options="infoOptions"
-                                            :position="{lat:user_latitude, lng:user_longitude}"
-                                            :opened="infoWinOpen"
-                                            @closeclick="infoWinOpen=false"
-                                        >あなたの現在地</GmapInfoWindow>
-                                        <GmapMarker
-                                        @click="toggleInfoWindow(0)"
+                                    <GmapInfoWindow
+                                        :options="infoOptions"
                                         :position="{lat:user_latitude, lng:user_longitude}"
-                                        :clickable="true">
-                                        </GmapMarker>
+                                        :opened="infoWinOpen"
+                                        @closeclick="infoWinOpen=false"
+                                    >あなたの現在地</GmapInfoWindow>
+                                    <GmapMarker
+                                    @click="toggleInfoWindow(0)"
+                                    :position="{lat:user_latitude, lng:user_longitude}"
+                                    :clickable="true">
+                                    </GmapMarker>
 
-                                        <GmapInfoWindow
-                                            :options="infoOptions"
-                                            :position="{lat:user_latitude, lng:user_longitude+0.0001}"
-                                            :opened="infoWinOpen2"
-                                            @closeclick="infoWinOpen2=false"
-                                        >配達者の現在地</GmapInfoWindow>
-                                        <GmapMarker
-                                        @click="toggleInfoWindow(1)"
+                                    <GmapInfoWindow
+                                        :options="infoOptions"
                                         :position="{lat:user_latitude, lng:user_longitude+0.0001}"
-                                        :clickable="true">
-                                        </GmapMarker>
-                                    </GmapMap>
-                                </v-card>
-                            </v-card>
-                        </v-col>
-                        <v-col cols="3" class="ma-0 pa-0">
-                            <v-card class="blue pa-3" height="200" elevation="0" tile>
-                                <v-row class="ma-0 pa-0" justify="center">
-                                    <v-col class="ma-0 pa-0" cols="auto">
-                                        <v-avatar color="indigo" size="180" class="ma-0 pa-0">
-                                            <span class="white--text headline">配達者の顔</span>
-                                        </v-avatar>
-                                    </v-col>
-                                </v-row>
-                            </v-card>
-                            <v-card  class="orange display-1 py-12" height="400" elevation="0" tile v-if="!tab">
-                                <v-row class="ma-0 pa-0" justify="center">
-                                    <v-col  class="ma-0 pa-0" cols="auto">
-                                        ニックネーム
-                                    </v-col>
-                                </v-row>
-                                <v-row class="ma-0 pa-0 my-12" justify="center">
-                                    <v-col  class="ma-0 pa-0" cols="auto">
-                                        到着予定時間
-                                    </v-col>
-                                </v-row>
-                                <v-row class="ma-0 pa-0" justify="center">
-                                    <v-col class="ma-0 pa-0" cols="auto">
-                                        <v-btn width="290" height="150" class="display-1" fab elevation="0" @click="change">
-                                            配達者と話す
-                                        </v-btn>
-                                    </v-col>
-                                </v-row>
-                            </v-card>
-                            <!-- 切替 -->
-                            <v-card class="green display-1 overflow-y-auto" id="takasa" height="350" elevation="0" tile v-else-if="tab">
-                                <v-card-text
-                                    class="body-1 pb-0 ma-0"
-                                    v-for="(item,index) in chat"
-                                    :key="item.content"
-                                    :index="index"
-                                >
-                                    ニックネーム:<v-card-text
-                                            class="title pt-0 ma-0">
-                                            {{item.content}}
-                                        </v-card-text>
-                                </v-card-text>
-                            </v-card>
-                            <v-card class="white display-1 overflow-y-auto pa-0 ma-0" height="50" elevation="0" v-if="tab">
-                                <v-row class="ma-0 pa-0 px-2">
-                                    <v-col cols="10" class="ma-0 pa-0 py-4">
-                                        <v-text-field
-                                            dense
-                                            label="Message"
-                                            class="ma-0"
-                                            v-model="coment"
-                                        ></v-text-field>
-                                    </v-col>
-                                    <v-col cols="2" class="ma-0 pa-0 py-1">
-                                        <v-btn class="pa-0 ma-0 ml-1" tile large color="teal" icon @click="send">
-                                            <v-icon class="pa-0 ma-0">mdi-send</v-icon>
-                                        </v-btn>
-                                    </v-col>
-                                </v-row>
-                            </v-card>
-                            <v-card  class="purple" height="100" elevation="0" tile>
-                                <v-row class="ma-0 pa-0" justify="center" style="height:100px;" align="center">
-                                    <v-col class="ma-0 pa-0" cols="auto">
-                                        <v-btn width="290" height="70" class="display-1" elevation="0">
-                                            配達完了
-                                        </v-btn>
-                                    </v-col>
-                                </v-row>
+                                        :opened="infoWinOpen2"
+                                        @closeclick="infoWinOpen2=false"
+                                    >配達者の現在地</GmapInfoWindow>
+                                    <GmapMarker
+                                    @click="toggleInfoWindow(1)"
+                                    :position="{lat:user_latitude, lng:user_longitude+0.0001}"
+                                    :clickable="true">
+                                    </GmapMarker>
+                                </GmapMap>
                             </v-card>
                         </v-col>
                     </v-row>
                 </v-col>
             </v-row>
+            <v-row class="ma-0 pa-0" justify="center">
+                <v-col class="ma-0 pa-0" cols="auto">
+                    <v-btn width="290" height="150" class="display-1" fab elevation="0" @click="change">
+                        配達者と話す
+                    </v-btn>
+                </v-col>
+            </v-row>
+            <v-row class="ma-0 pa-0" justify="center" style="height:100px;" align="center">
+                <v-col class="ma-0 pa-0" cols="auto">
+                    <v-btn width="290" height="70" class="display-1" elevation="0">
+                        配達完了
+                    </v-btn>
+                </v-col>
+            </v-row>
+            <v-overlay
+                :opacity="opacity"
+                :value="overlay"
+                >
+                <v-row class="ma-0 pa-0" align="end">
+                    <v-col cols="auto">
+                        <v-btn
+                            color="orange lighten-2"
+                            @click="overlay = false"
+                            tile
+                        >
+                            Mapに戻る
+                        </v-btn>
+                        <v-card class="display-1 overflow-y-auto" max-height="500" max-width="375" elevation="0" tile>
+                            <v-card-text
+                                v-for="(item,index) in chat"
+                                :key="item.content"
+                                :index="index"
+                            >
+                                ニックネーム:<v-card-text
+                                        class="title pt-0 ma-0">
+                                        {{item.content}}
+                                    </v-card-text>
+                            </v-card-text>
+                        </v-card>
+                        
+                        <v-card class="white black--text display-1 py-2 pl-5 ma-0" outlined max-height="60" elevation="0">
+                            <v-row class="ma-0 pa-0" justify="end">
+                                <v-col cols="10" class="ma-0 pa-0">
+                                    <v-text-field
+                                        label="Message"
+                                        class="ma-0"
+                                        color="black"
+                                        background-color="white"
+                                        v-model="coment"
+                                    ></v-text-field>
+                                </v-col>
+                                <v-col cols="2" class="ma-0 pa-0 py-1">
+                                    <v-btn class="pa-0 ma-0 ml-1" tile large color="teal" icon @click="send">
+                                        <v-icon class="pa-0 ma-0">mdi-send</v-icon>
+                                    </v-btn>
+                                </v-col>
+                            </v-row>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </v-overlay>
         </v-flex>
     </v-layout>
 </template>
@@ -125,8 +114,8 @@ export default {
         return {
             infoOptions: {
                 pixelOffset: {
-                width: 0,
-                height: -35
+                    width: 0,
+                    height: -35
                 }
             },
             infoWindowPos: null,
@@ -146,11 +135,14 @@ export default {
                     // カスタマイズで使用したスタイルなどはここに。
                 ]
             },
-            tab:true,
-            chat:[]
+            tab:false,
+            chat:[],
             // marker_items: [
             //     { position: { lat: YOUR_lat, lng: YOUR_lng }, title: 'title' }
-            // ]
+            // ],
+            absolute: true,
+            opacity: 0.4,
+            overlay: true,
         }
     },
     methods:{
@@ -167,7 +159,7 @@ export default {
             // console.log("markerClick")
         },
         change:function(){
-            this.tab = !this.tab
+            this.overlay = !this.overlay
         },
         send:function(){
             // this.chat = []
