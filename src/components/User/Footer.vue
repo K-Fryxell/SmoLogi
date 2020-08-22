05ed37d5ceee1aabbfa79d3e224f00f0e100a6d9
iwakurakaito <kangaruuuu7@docomo.ne.jp>
2020-08-22 22:55:48 +0900

    iwakura_restart0822

--- src/components/User/Footer.vue
+++ src/components/User/Footer.vue
@@ -6,7 +6,7 @@
 					<v-row class="ma-0 pa-0" justify="center">
 						<v-col class="ma-0 pa-0">
 							<v-row justify="center">
-								<v-btn  to="#" height="27" width="20"
+								<v-btn  to="/terms" height="27" width="20"
 									class="overline lighten-1" :elevation="0" color="white" text>
 									利用規約
 								</v-btn>
@@ -21,14 +21,14 @@
 						</v-col>
 						<v-col class="ma-0 pa-0">
 							<v-row justify="center">
-								<v-btn to="/terms" height="27" width="40"  class="overline lighten-1" :elevation="0" color="white" text>
+								<v-btn to="/inquery" height="27" width="40"  class="overline lighten-1" :elevation="0" color="white" text>
 									ヘルプ
 								</v-btn>
 							</v-row>
 						</v-col>
 						<v-col class="ma-0 pa-0">
 							<v-row justify="center">
-								<v-btn to="/inquery" height="27" width="40" class="overline lighten-1" :elevation="0" color="white" text>
+								<v-btn to="#" height="27" width="40" class="overline lighten-1" :elevation="0" color="white" text>
 									お問い合わせ
 								</v-btn>
 							</v-row>
